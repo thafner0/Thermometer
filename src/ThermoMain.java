13c34998a331e450091de78e4f79a4ec9526c007
Trevor Hafner <thafner777lr@gmail.com>
2022-12-09 15:38:58 -0600

    Adds dummy methods to act as stand in prior to the actual code being ready.

--- src/ThermoMain.java
+++ src/ThermoMain.java
@@ -1,5 +1,13 @@
 public class ThermoMain {
     public static void main(String[] args) {
-        
+
+    }
+
+    public String jokeQuestion() {
+        return "A block fell on Isaac Newton's Head. What did he say to laughing his assistant?";
+    }
+
+    public String jokePunchLine() {
+        return "I don't think you understand the gravity of the situation.";
     }
 }
