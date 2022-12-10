5210c9a58c71784bb58391e1872aed63e631d2e0
oyane_5ab4y9h <oyanek@gmail.com>
2022-12-10 14:13:43 -0600

    DisplayScreen finished

--- src/main/DisplayScreen.java
+++ src/main/DisplayScreen.java
@@ -1,9 +1,14 @@
-
-
 public class DisplayScreen {
     private int tempDisplay = 0;
+    private int batt = 0;
 
     public DisplayScreen(int temp){
-        tempDisplay = temp;
+        tempDisplay = this.temp;
+        batt = SelfTestSystem.getBattLevel();
+    }
+
+    public void updateScreen(){
+        System.out.println("temp is: "+tempDisplay);
+        System.out.println("batt is: "+batt);
     }
 }
