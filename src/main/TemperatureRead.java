21e9f5341a146a99c1ee38443d694c20f62c2ce1
Sara <71234434+sarag548@users.noreply.github.com>
2022-12-10 23:13:11 -0600

    Added TemperatureRead.java

--- src/main/TemperatureRead.java
+++ src/main/TemperatureRead.java
@@ -1,12 +1,20 @@
 public class TemperatureRead {
     private double avgTemp = 0;
+
     private boolean readTemp;
-    public Double calcAvgTemp(Double temp1, Double temp2, Double temp3, Double temp4, Double temp5, Double temp6, Double temp7, Double temp8, Double temp9, Double temp10) {
+
+    private long lastUpdateTime;
+
+    public long getTime(){
+        return lastUpdateTime;
+    }
+    public double calcAvgTemp(double temp1, double temp2, double temp3, double temp4, double temp5, double temp6, double temp7, double temp8, double temp9, double temp10) {
         avgTemp = (temp1 + temp2 + temp3 + temp4 + temp5 + temp6 + temp7 + temp8 + temp9 + temp10) / 10;
         new DisplayScreen ds = DisplayScreen(avgTemp);
+        lastUpdateTime = System.currentTimeMillis();
     }
 
-    public String errorMessage(Double temp) {
+    public String errorMessage(double temp) {
         if (temp < -58 || temp > 122) {
             System.out.println("TMP ERR");
         }
