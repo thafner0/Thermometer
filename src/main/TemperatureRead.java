d011308e9beb8135ba96cbd711e465948a202faa
Sara <71234434+sarag548@users.noreply.github.com>
2022-12-11 16:22:29 -0600

    Edited TemperatureRead.java

--- src/main/TemperatureRead.java
+++ src/main/TemperatureRead.java
@@ -1,14 +1,16 @@
 public class TemperatureRead {
     private double avgTemp = 0;
 
-    private long lastUpdateTime;
+    private long lastUpdateTime = 0;
 
     public long getTime(){
         return lastUpdateTime;
     }
 
+    public void readTemp(){
 
-    public double calcAvgTemp(double temp1, double temp2, double temp3, double temp4, double temp5, double temp6, double temp7, double temp8, double temp9, double temp10) {
+    }
+    public void calcAvgTemp(double temp1, double temp2, double temp3, double temp4, double temp5, double temp6, double temp7, double temp8, double temp9, double temp10) {
         avgTemp = (temp1 + temp2 + temp3 + temp4 + temp5 + temp6 + temp7 + temp8 + temp9 + temp10) / 10;
         new DisplayScreen ds = DisplayScreen(avgTemp);
         lastUpdateTime = System.currentTimeMillis();
