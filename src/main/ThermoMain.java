79d5c5de4f6f5a6783c73cb49a5907abb745c014
ademange1 <ademange@luc.edu>
2022-12-11 22:15:41 -0600

    fixed number input working if system test failed

--- src/main/ThermoMain.java
+++ src/main/ThermoMain.java
@@ -4,30 +4,28 @@ import java.util.ArrayList;
 
 public class ThermoMain {
     public static void main(String[] args) {
-        //call power on method
-        //check to see if powered on
-        //call self check
-        //perform temperature read
         ThermometerState t = new ThermometerState();
         t.powerOn();
 
-        Scanner sc = new Scanner(System.in);
-        int validInput = 0;
+        if (t.isOn()) {
+            Scanner sc = new Scanner(System.in);
+            int validInput = 0;
 
-        ArrayList <Double> temperatures = new ArrayList<Double>();
-        while(validInput < 10){
-            double num = 0;
-            System.out.print("Input next temperature (" + (validInput+1) + "/10):");
-            try{
-                num = Double.parseDouble(sc.next());
-                temperatures.add(num);
-                validInput++;
-            } catch (NumberFormatException ignore) {
-                System.out.println("Input must be a double, please try again");
+            ArrayList<Double> temperatures = new ArrayList<Double>();
+            while (validInput < 10) {
+                double num = 0;
+                System.out.print("Input next temperature (" + (validInput + 1) + "/10):");
+                try {
+                    num = Double.parseDouble(sc.next());
+                    temperatures.add(num);
+                    validInput++;
+                } catch (NumberFormatException ignore) {
+                    System.out.println("Input must be a double, please try again");
+                }
             }
+            TemperatureRead tr = new TemperatureRead();
+            tr.calcAvgTemp(temperatures);
         }
-        TemperatureRead tr = new TemperatureRead();
-        tr.calcAvgTemp(temperatures);
     }
 
     //remove these when tests are updated
