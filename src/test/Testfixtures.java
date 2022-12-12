a0098561c10a01061097ab120be8f819fb37c0d5
ademange1 <ademange@luc.edu>
2022-12-11 20:42:11 -0600

    Updated main with user input

--- src/test/Testfixtures.java
+++ src/test/Testfixtures.java
@@ -13,6 +13,10 @@ public class Testfixtures {
         temperatures1.add(97.3);
         temperatures1.add(97.2);
         temperatures1.add(97.3);
+        temperatures1.add(97.2);
+        temperatures1.add(97.3);
+        temperatures1.add(97.3);
+        temperatures1.add(97.4);
 
         temperatures2.add(20.1);
         temperatures2.add(20.3);
@@ -20,11 +24,20 @@ public class Testfixtures {
         temperatures2.add(20.3);
         temperatures2.add(20.2);
         temperatures2.add(20.2);
+        temperatures2.add(20.1);
+        temperatures2.add(20.0);
+        temperatures2.add(19.9);
+        temperatures2.add(20.0);
 
         temperatures3.add(18.9);
         temperatures3.add(78.1);
         temperatures3.add(47.91);
         temperatures3.add(86.56);
         temperatures3.add(18.9);
+        temperatures3.add(65.9);
+        temperatures3.add(78.1);
+        temperatures3.add(47.91);
+        temperatures3.add(86.56);
+        temperatures3.add(16.29);
     }
 }
